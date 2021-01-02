4f10dbc43cf7c8c2efb952af7f7dca523c5c48f3
Rasyidf <ufarasfa@gmail.com>
2021-01-02 13:33:11 +0700

    UpdateTab

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,18 +11,18 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+       <ion-button>
+         Login
+       </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'; 
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {  IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton }
 }
 </script>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -6,14 +6,14 @@
           <ion-icon :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-          
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
+              
+      <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-icon :icon="square" />
+        <ion-label>Photos</ion-label>
+      </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="ellipse" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
